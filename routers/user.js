25038d23ac6f53a6d4ca42f98a2c486c63e325a4
Your Name <you@example.com>
2022-11-21 07:53:11 +0500

    my donee

--- routers/user.js
+++ routers/user.js
@@ -16,7 +16,7 @@ const e = require('express');
 router.get('/user/:id',async(req,res) => {
     try {
         let user = await User.find({ id: req.params.id })
-        console.log("user after find", user)
+        // console.log("user after find", user)
         res.status(200).json({success: true, user: user })
     } catch (error) {
         console.log("error in getting user")
@@ -29,30 +29,15 @@ router.get('/user/:id',async(req,res) => {
 router.put('/user/:id',async(req,res) => {
     try {
         let update = req.body
-        let user = await User.findByIdAndUpdate(id, update )
-        console.log("user after findByIdAndUpdate", user)
+        let user = await User.findByIdAndUpdate(req.params.id, update, { new: true } )
+        // console.log("user after findByIdAndUpdate", user)
         res.status(200).json({success: true, user: user })
     } catch (error) {
-        console.log("error in getting user")
+        console.log("error in updating user info",error )
         res.status(400).json({error: "error in getting user info"})
     }
 });
-
-
-
-
-
-
-
-router.get('/myposts',(req,res)=>{res.send('POST A JOB FORML FILL KRNY K BAD YAHA SHOW HONA CHAIE USY USKI JOB POSTS ');});
-
-router.get('/settings',(req,res)=>{ res.send('change password');
-res.send('change email address');res.send('update my profile');});
-   
-router.get('/messages',(req,res)=>{ res.send('hello from messages'); });  
-
-// router.get('/signup',(req,res)=>{ res.send('hello from signup'); });    
-//  router.get('/login',middleware(),(req,res)=>{ res.send('hello from login'); });    
+  
 
 module.exports = router;
 
